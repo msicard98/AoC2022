c80a28c644ded46f32730b171444972ed1cebf2d
Meg Sicard <megsicard@macbook>
2022-12-01 18:41:48 -0500

    added comments

--- day1/a.py
+++ day1/a.py
@@ -1,12 +1,18 @@
+# read input from text file
 f = open("/Users/meg/Documents/AoC2022/day1/input.txt")
-
 input = f.read()
+
+# split on different elfs (sections of numbers)
 elfs = input.split("\n\n")
 
 max = 0
 for elf in elfs: 
+    # split on each calorie count for this elf
     calories = elf.split("\n")
+    # get total calories for this elf
     total = sum([eval(i) for i in calories])
+    # if this elf has the most so far, set to max
     if total > max:
         max = total
+# print answer
 print(max)
--- day1/b.py
+++ day1/b.py
@@ -1,14 +1,20 @@
+# read input from text fileS
 f = open("/Users/meg/Documents/AoC2022/day1/input.txt")
-
 input = f.read()
+
+# split on different elfs (sections of numbers)
 elfs = input.split("\n\n")
 
+# keep track of the top 3 totals
 max1 = 0
 max2 = 0
 max3 = 0
 for elf in elfs: 
+    # split on each calorie count for this elf
     calories = elf.split("\n")
+    # get total calories for this elf
     total = sum([eval(i) for i in calories])
+    # if this elf has more than any of the top 3, set and demote others
     if total > max1: 
         max3 = max2
         max2 = max1
@@ -18,4 +24,5 @@ for elf in elfs:
         max2 = total
     elif total > max3 and total < max2:
         max3 = total
+# print answer
 print(sum([max1,max2,max3]))
