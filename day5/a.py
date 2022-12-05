1a0e758da41920f9b366c951b4de79c0c5cd35e0
Meg Sicard <megsicard@macbook>
2022-12-05 12:01:55 -0500

    day5 submission

---
+++ day5/a.py
@@ -0,0 +1,29 @@
+f = open("/Users/meg/Documents/AoC2022/day5/input.txt")
+
+# create stacks 
+stacks = [
+    ["S","L","W"],
+    ["J","T","N","Q"],
+    ["S","C","H","F","J"],
+    ["T","R","M","W","N","G","B"],
+    ["T","R","L","S","D","H","Q","B"],
+    ["M","J","B","V","F","H","R","L"],
+    ["D","W","R","N","J","M"],
+    ["B","Z","T","F","H","N","D","J"],
+    ["H","L","Q","N","B","F","T"]
+]
+
+# move crates
+for line in f:
+    moves = [int(i) for i in line.split() if i.isdigit()]
+    #Sprint(moves)
+    for i in range(int(moves[0])):
+        crate = stacks[moves[1]-1].pop()
+        stacks[moves[2]-1].append(crate)
+
+# print answer 
+for stack in stacks: 
+    print(stack.pop())
+
+# wrong answer
+#NTLBBLMJT
